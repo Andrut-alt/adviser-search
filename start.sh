94781540320cf2a589e332dade6b1683cafbc573
Andrut-alt <voloshyn.andriy2006@gmail.com>
2025-12-07 16:17:56 +0200

    Add entrypoint script

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo "-------- STARTING DEPLOYMENT SCRIPT --------"
+
+echo "--> Applying database migrations..."
+python manage.py migrate
+
+echo "--> Creating superuser (if needed)..."
+python create_superuser.py
+
+echo "--> Starting Gunicorn Server..."
+
+gunicorn mentorion.wsgi:application
